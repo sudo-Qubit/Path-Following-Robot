395a53b66bbefa269fe49b0edf993407fbf8a39e
Mohamed <sudo-Qubit@github.com>
2025-06-17 13:33:37 +0200

    Add Lidar to the robot

--- build/robot_description/prefix_override/sitecustomize.py
+++ build/robot_description/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/the/simulation_ws/install/robot_description'
+    sys.prefix = sys.exec_prefix = '/home/mazino/Path-Following-Robot/install/robot_description'
--- build/teleop_twist_keyboard/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/the/simulation_ws/install/teleop_twist_keyboard'
