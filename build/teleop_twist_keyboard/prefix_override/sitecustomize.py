4365abfba10d4263cd458e8e8cee6ad5a962670e
Mohamed Oubelaid <m.oubelaid001@gmail.com>
2025-06-26 16:24:58 +0400

    Add IMU to Robot

--- build/teleop_twist_keyboard/prefix_override/sitecustomize.py
+++ build/teleop_twist_keyboard/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mazino/Path-Following-Robot/install/teleop_twist_keyboard'
+    sys.prefix = sys.exec_prefix = '/home/enryu/Desktop/Path-Following-Robot/install/teleop_twist_keyboard'
